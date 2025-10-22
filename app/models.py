81eb0efe18bbadcdeb04ddbd1c2f7a483a994d79
Krzysztof Zdebski <km.zdebski@gmail.com>
2025-10-22 08:39:50 +0200

    db

--- app/models.py
+++ app/models.py
@@ -4,16 +4,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
-
-    def __repr__(self):
-        return f'<Task {self.title}>'
