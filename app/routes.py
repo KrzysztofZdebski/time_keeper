7621ea14449a5e0f995ebf669bb3b5fba13ccfe6
mmiszczynski <matimiszcz3@gmail.com>
2025-10-22 11:07:28 +0200

    test

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import User, db
+from werkzeug.security import check_password_hash
 
-main = Blueprint('main', __name__)
+bp = Blueprint('routes', __name__)
 
-@main.route('/')
-def home():
-    return "Hello, World!"
+@bp.route('/')
+def index():
+    return redirect(url_for('routes.login'))
 
-@main.route('/about')
-def about():
-    return "About Page"
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password):
+            return redirect(url_for('routes.dashboard'))
+        else:
+            flash('Nieprawidłowy email lub hasło', 'error')
+            return redirect(url_for('routes.login'))
+
+    return render_template('login.html')
+
+@bp.route('/dashboard')
+def dashboard():
+    return "<h2>Witaj w panelu użytkownika!</h2>"
