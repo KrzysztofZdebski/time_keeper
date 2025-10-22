0cde17279070b7f2785f56f8559345b9298795c8
Krzysztof Zdebski <km.zdebski@gmail.com>
2025-10-22 08:25:14 +0200

    app struct

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from .config import Config
+from .extensions import db
+from .routes import main
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        from . import routes
+
+    app.register_blueprint(main)
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
