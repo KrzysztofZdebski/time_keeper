587947358b3343af78f93f7e66a92a437381244d
mmiszczynski <matimiszcz3@gmail.com>
2025-10-25 14:54:24 +0200

    update

--- app/models.py
+++ app/models.py
@@ -8,4 +8,15 @@ class User(db.Model):
     password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.email}>'
+    
+class SessionEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=True)
+
+    user = db.relationship('User', backref=db.backref('sessions', lazy=True))
+
+    def __repr__(self):
+        return f'<SessionEntry {self.id} for User {self.user_id} from {self.start_time} to {self.end_time}>'
